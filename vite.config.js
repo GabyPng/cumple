87e67c467b593411994ed2e9dd3ad9ae429b595d
GabyPng <[gaby.paniaguag@gmail.com](GabyPng:gaby.paniaguag@gmail.com)>
2025-09-21 00:51:48 -0600

    Configure deployment settings

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Cumple/',  // Reemplaza 'Cumple' con el nombre de tu repositorio
-}){ defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+  base: '/cumple/'
 })
